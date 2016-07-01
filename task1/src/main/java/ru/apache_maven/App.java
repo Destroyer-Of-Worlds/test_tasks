9987497fb4333c07d8d4adbf000a4a5075cedaf2
Destroyer-of-Worlds <welcometogloomworld@yandex.ru>
2016-07-02 00:55:15 +0300

    almost done

--- task1/src/main/java/ru/apache_maven/App.java
+++ task1/src/main/java/ru/apache_maven/App.java
@@ -2,47 +2,68 @@ package ru.apache_maven;
 
 
 import java.util.Scanner;
-import java.util.Vector;
-
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 class PhoneBook
 {
-	String m_name = new String("");
-	Vector m_phones = new Vector();
+	private Map<String, ArrayList> m_rec = new HashMap<String, ArrayList>();
 	
-	PhoneBook(String name, Vector phones)
+	PhoneBook(String name, ArrayList phones)
 	{
-		m_name += name;
-		m_phones.addAll(phones);
+		m_rec.put(name, phones);
 		
 	}
 	public void parseAndSet(String data)
 	{
-		String[] tmp = data.split(" +");
-		m_name = tmp[0];
+		String[] tmp = data.split("\\+");
+		String name = tmp[0].trim();
+		ArrayList phones = new ArrayList();
 		for (int i = 1; i <= tmp.length-1; ++i)
 		{
-		    m_phones.add(tmp[i]);
+		    phones.add("+"+tmp[i].trim());
 		}
+		m_rec.put(name, phones);
 		
 	}
 	public void printPhone(){
-		System.out.println(m_name + " "); 
+	    for (Map.Entry entry : m_rec.entrySet()) 
+	    {
+           System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+ 
 	}
-	//serch()
+	public void findByName(String name)
+	{
+	    
+	    if (m_rec.containsKey(name))
+	        System.out.println(m_rec.get(name));
+	        
+	   else
+	    System.out.println(name + " not found");
+	}
 }
-
 
 public class App 
 {
     public static void main( String[] args )
     {
-	Scanner sc = new Scanner(System.in);
-        System.out.println( "Enter data " );
-	String s;
-	s = sc.nextLine();
-	PhoneBook p = new PhoneBook("", new Vector());
-	p.parseAndSet(s);
+	
+	PhoneBook p = new PhoneBook("", new ArrayList());
+
+	p.parseAndSet("Papandopulos Eugenie +888 666");
+	p.parseAndSet("Harry Potter +123 345 678 +987 765 555");
+	p.parseAndSet("Darth Wayder +666 666 666");
+	p.parseAndSet("Pupkin V. V. +1234567890 +56789");
+	p.parseAndSet("Sharikov P. P. +11111 +1111 +111111");
+	p.parseAndSet(" Albus Percival Wulfric Brian Dumbledore +lemon sherbet");//TODO phoneNumber check
 	p.printPhone();
+	
+	Scanner sc = new Scanner(System.in);
+	String s;
+	System.out.println( "Enter data " );
+    s = sc.nextLine();
+    p.findByName(s);
     }
 }
