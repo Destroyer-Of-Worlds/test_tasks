4372dfe4be76cb04c94bf831282b90926e649dab
Destroyer-of-Worlds <welcometogloomworld@yandex.ru>
2016-07-01 11:52:40 +0300

    .

--- task1/src/main/java/ru/apache_maven/App.java
+++ task1/src/main/java/ru/apache_maven/App.java
@@ -1,6 +1,36 @@
 package ru.apache_maven;
 
+
 import java.util.Scanner;
+import java.util.Vector;
+
+
+class PhoneBook
+{
+	String m_name = new String("");
+	Vector m_phones = new Vector();
+	
+	PhoneBook(String name, Vector phones)
+	{
+		m_name += name;
+		m_phones.addAll(phones);
+		
+	}
+	public void parseAndSet(String data)
+	{
+		String[] tmp = data.split(" +");
+		m_name = tmp[0];
+		for (int i = 1; i <= tmp.length-1; ++i)
+		{
+		    m_phones.add(tmp[i]);
+		}
+		
+	}
+	public void printPhone(){
+		System.out.println(m_name + " "); 
+	}
+}
+
 
 public class App 
 {
@@ -10,6 +40,8 @@ public class App
         System.out.println( "Enter data " );
 	String s;
 	s = sc.nextLine();
-	System.out.println( s );
+	PhoneBook p = new PhoneBook("", new Vector());
+	p.parseAndSet(s);
+	p.printPhone();
     }
 }
