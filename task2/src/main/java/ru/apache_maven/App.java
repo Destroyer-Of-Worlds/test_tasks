e7b6966c1528ef82834e0f1893e7d2b70eb1ee6f
Destroyer-of-Worlds <welcometogloomworld@yandex.ru>
2016-07-03 16:48:38 +0300

    Add incIP(), printAllIPTo(IP ip), check format IP

--- task2/src/main/java/ru/apache_maven/App.java
+++ task2/src/main/java/ru/apache_maven/App.java
@@ -18,8 +18,12 @@ class IP
 		for (int i = 0; i < 4; ++i)
 		{
 			m_ip[i] = (int)Integer.valueOf(buf[i]);
+			if (m_ip[i] < 0 || m_ip[i] > 255)
+			{
+				System.out.println("Wrong format of IP");
+				System.exit(0);
+			}
 		}
-		//m_ip.addAll(Integer.valueOf());
 	}
 
 	public void printIP()
@@ -28,9 +32,34 @@ class IP
 			System.out.print(m_ip[i] + ".");
 		System.out.println(m_ip[3]);
 	}
+	
 	public void incIP()
 	{
-		
+		int buf = 1;
+		for (int i = 3; i >= 0; --i)
+		{
+			m_ip[i]+=buf;
+			buf = m_ip[i] / 256;
+			m_ip[i] %= 256;
+		}
+	}
+
+	public boolean equalIP(IP ip)
+	{
+		boolean flag = true;
+
+		for (int i = 0; i < 4; ++i)
+			flag = flag && (ip.m_ip[i] == this.m_ip[i]);
+		return flag;
+	}
+
+	public void printAllIPTo(IP endIP)
+	{
+		while (! this.equalIP(endIP))
+		{
+			this.incIP();
+			this.printIP();
+		}
 	}	
 }
 
@@ -38,8 +67,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        IP p = new IP();
-	p.parse("123.333.333.333");
-	p.printIP();
+	Scanner sc = new Scanner(System.in);
+	
+        IP p1 = new IP();
+	IP p2 = new IP();
+	System.out.print("Enter Begin ");
+	String s1;
+	s1 = sc.nextLine();
+	p1.parse(s1);	
+	System.out.print("Enter End ");
+	String s2;	
+	s2 = sc.nextLine();
+	p2.parse(s2);
+	p1.printAllIPTo(p2);
+
     }
 }
